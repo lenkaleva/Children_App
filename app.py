ffd5ccc7962810c92b93819c8c94fe8e20437156
lenkaleva <74210071+lenkaleva@users.noreply.github.com>
2025-11-28 14:17:43 +0100

    Set up Streamlit app with data loading
    Initialize Streamlit app and load data from CSV.

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# Import stránky Lenka
+#from pages.lenka import show_lenka_page
+#show_lenka_page()
+
+
+# loading data - to be used na jednotlivych pages
+# oprava 2 Belgium na jednotny stat
+if 'df' not in st.session_state:
+    st.session_state.df = pd.read_csv('data.csv')
+   
+df = st.session_state.df
+
+
+
+
+
+
+
+
+
+
